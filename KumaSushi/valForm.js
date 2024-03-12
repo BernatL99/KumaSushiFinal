a10f9a7a4d0733e986e9ec572e2cb704676a715d
BernatL99 <116670667+BernatL99@users.noreply.github.com>
2024-03-12 17:24:01 +0100

    JS and NewCSS
    Agregamos JS y separamos CSS a uno nuevo para que sea más entendible

--- KumaSushi/valForm.js
+++ KumaSushi/valForm.js
@@ -1,8 +1,25 @@
 function validacion(){
-    var num = document.getElementById(NumTelf);
-    var mail = document.getElementById(correo);
-    if(mail != /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-z]{2,6}$/){
-        alert("¡¡¡El correo no es correcto!!!");
+    var name = document.getElementById("NameForm");
+    var SecName = document.getElementById("apellidos");
+    var num = document.getElementById("NumTelf");
+    var mail = document.getElementById("correo");
+    
+    if(!/^[A-Za-z]+/.test(name)){
+        alert("El nombre tiene que tener mínimo 3 carácteres");
+        return false;
     }
+    if(!/^[A-Za-z ]+/.test(SecName)){
+        alert("El campo de apellido tiene que tener mínimo 3 carácteres");
+        return false;
+    }
+    if(!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-z]{2,6}$/.test(mail)){
+        alert("La dirección de correo eléctronico no és válida");
+        return false;
+    }
+    if(!/^[0-9]{9}/.test(num)){
+        alert("El numero de teléfono no es válida")
+        return false;
+    }
+    return true;
     
 }
